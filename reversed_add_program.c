780e9c516ac74a3bb3282030fbf9a6256fb0797a
Mubtasim Ahmed Rakheen <mubtasimahmed.rakheen@mavs.uta.edu>
2021-09-11 12:35:57 -0500

    Update reversed_add_program.c

--- reversed_add_program.c
+++ reversed_add_program.c
@@ -1,16 +1,3 @@
-//Reverse_Adding_C 
-//Done Using C Language
-
-/***You are given two non-empty linked lists representing two non-negative integers. The digits are
-stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return
-the sum as a linked list. You may assume the two numbers do not contain any leading zero, except the number 0 itself.
-0 <= Value of Each Array Element <= 9 (single digit array elements) 
-0 <= Value of array element <= 9
-***/
-
-/***Example: Input: l1 = [2,4,3], l2 = [5,6,4]
-            Output: [7,0,8]
-            Explanation: 342 + 465 = 807.***/
 
 #include<stdio.h>
 #include<stdlib.h>
